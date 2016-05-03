dc94749fb5076ed9050ab7270a5a47e536aa8ff3
duyalei <3209742516@qq.com>
2016-05-03 17:29:47 +0800

    添加了PC端演示的大致样式

--- demo/js/image360.js
+++
@@ -1,6 +0,0 @@
-(function ($) {
-	// 代码开始
-    $.fn.image360 = function () {
-        
-    };
-})(jQuery);
---
+++ demo/js/jquery.image360.js
@@ -0,0 +1,16 @@
+(function ($) {
+	// 代码开始
+    $.fn.image360 = function (options) {
+
+    	// 插件的默认参数
+        var defaults = {
+		    
+		};
+
+		// 设定参数的覆盖顺序
+		var settings = $.extend( {}, defaults, options);
+
+		
+
+    };
+})(jQuery);
